c85615d6ed58461373a18f8654aed8847c187fa6
ronaldcoe-snow <104688933+ronaldcoe-snow@users.noreply.github.com>
2022-10-20 12:01:52 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+  import snowflake.connector
+  import streamlit
+  
+  
+  
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
+    streamlit.text(back_from_function)
