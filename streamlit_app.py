1964e0b1d37fbdcf23e473b4a4548bed15465fb6
ronaldcoe-snow <104688933+ronaldcoe-snow@users.noreply.github.com>
2022-10-20 12:04:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
   import snowflake.connector
   import streamlit
   
-  
+  def get_demo_table_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM DEMO_TABLE")
+        return my_cur.fetchall()
+
   
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
+    back_from_function = get_demo_table_list()
     my_cnx.close()
     streamlit.text(back_from_function)
