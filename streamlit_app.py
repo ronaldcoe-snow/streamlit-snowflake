ddae802a323c30d8b62262f9127405d707d0a5d8
ronaldcoe-snow <104688933+ronaldcoe-snow@users.noreply.github.com>
2022-10-20 13:12:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import snowflake.connector
   
 def get_demo_table_list():
   with my_cnx.cursor() as my_cur:
-      my_cur.execute("SELECT * FROM DEMO_TABLE")
+      my_cur.execute("SELECT * FROM demo_schema.DEMO_TABLE")
       return my_cur.fetchall()
 
   
