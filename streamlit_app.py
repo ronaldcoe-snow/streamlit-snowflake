0e708aa6d2f5e19155dec9b66580bf79df4e8d7e
ronaldcoe-snow <ronald.coe@snowflake.com>
2022-11-09 01:51:57 +0000

    working with filters

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ streamlit.table(df_months_represented.drop_duplicates(subset='transactionMonth')
 streamlit.table(df_months_represented.drop_duplicates(subset='transactionYear'))
 
 df_sl_years = df_months_represented['transactionYear'].drop_duplicates()
-df_sl_years_0 = df_sl_years.to_frame()
+df_sl_years_0 = df_sl_years.to_frame().reset_index()
 df_sl_years_0.set_index(['transactionYear'], inplace=True)
 
 streamlit.table(df_sl_years_0)
